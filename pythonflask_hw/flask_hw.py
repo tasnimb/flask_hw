af72448fa72c67f02240d1fe9a1485052573ce51
Michelle <mistrymichelle@gmail.com>
2021-04-06 19:17:31 +0100

    slight changes

--- pythonflask_hw/flask_hw.py
+++ pythonflask_hw/flask_hw.py
@@ -2,21 +2,23 @@ from flask import Flask, Response, request, url_for, render_template
 
 app = Flask(__name__)
 
+# the decorator(a wrapper function) uses flask to bind the url to the function
+
+
 @app.route('/')
 def hello_from_flask():
     return render_template('webpage.html')
-
 
 
 @app.route('/dynamic/<word>')
 def home(word):
     return word
 
-
 # @app.route('/dynamic/greeting/<name>')
 # def greet_name(name):
 #     new_name = "Hello " + name
 #     return new_name
+
 
 @app.route('/greet/<name>')
 def greet(name):
@@ -29,11 +31,11 @@ def greet(name):
 <body>
     <h1> Name page</h1>
     <p> Hello {} !</p>
-   
 
 </body>
 </html>
 """.format(name)
+
 
 @app.route('/get/text')
 def get_text():
@@ -41,8 +43,8 @@ def get_text():
 
 
 @app.route('/greet/<name>/<int:age>')
-def greet_age(name,age):
-    url=url_for('get_text')
+def greet_age(name, age):
+    url = url_for('get_text')
     return """
 <html>
 <head>
@@ -56,15 +58,15 @@ def greet_age(name,age):
     <hr>
     <a href='{}'> Welcome </a>
 
-
 </body>
 </html>
-""".format(name,age,url)
+""".format(name, age, url)
 
 
 @app.route('/contact_us')
 def contact():
     return "email us at gmail.com"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
